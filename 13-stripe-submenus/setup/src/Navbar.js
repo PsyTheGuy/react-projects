db4fc6ce34373f21ed9cb0fb2a09ef5e9f59b7a6
András Körmendi <psyplaysgames@gmail.com>
2023-01-31 20:02:16 +0100

    solved exercise 13

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,55 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-export default Navbar
+  const showSubmenu = (e) => {
+    const text = e.target.textContent;
+    const { width, bottom, left } = e.target.getBoundingClientRect();
+    const center = left + width / 2;
+    const top = bottom - 3;
+    openSubmenu(text, { center, top });
+  };
+
+  const hideSubmenu = (e) => {
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
+  };
+
+  return (
+    <nav className="nav" onMouseOver={hideSubmenu}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img className="nav-logo" src={logo} alt="logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={(e) => showSubmenu(e)}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={(e) => showSubmenu(e)}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={(e) => showSubmenu(e)}>
+              company
+            </button>
+          </li>
+        </ul>
+      </div>
+      <button className="btn signin-btn">Sign in</button>
+    </nav>
+  );
+};
+
+export default Navbar;
--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,47 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    location,
+    page: { page, links },
+    isSubmenuOpen,
+    closeSubmenu,
+  } = useGlobalContext();
+  const [columns, setColumns] = useState(links.length);
+  const container = useRef(null);
 
-export default Submenu
+  useEffect(() => {
+    const submenu = container.current;
+    submenu.style.top = `${location.top}px`;
+    submenu.style.left = `${location.center}px`;
+  }, [location]);
+
+  useEffect(() => {
+    setColumns(links.length);
+    console.log(container.current);
+  }, [page]);
+
+  return (
+    <aside
+      className={`submenu ${isSubmenuOpen && "show"}`}
+      onMouseLeave={closeSubmenu}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center col-${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
